9f963c5265e87e59fb6a423899316268f8fedadf
Muhammad Khubaib Ahmad <muhammadkhubaibahmad854@gmail.com>
2026-01-03 09:18:27 +0500

    ecapa_model loading

--- model/ecapa.py
+++ model/ecapa.py
@@ -1,16 +1,23 @@
 import torch
 import torchaudio
 from speechbrain.lobes.models.ECAPA_TDNN import ECAPA_TDNN
-from app.core.config import MODEL_DIR, DEVICE
+from app.core.config import MODEL_DIR, DEVICE, N_MELS
 import numpy as np
 
 class ECAPAENCODER:
     def __init__(self):
-        self.ecapa = torch.load(MODEL_SIR, map_location=DEVICE)
+        self.ecapa = ECAPA_TDNN(
+            input_size = N_MELS,
+            lin_neurons = 192,
+            channels = [512, 512, 512],
+            kernel_sizes = [5, 3, 3],
+            dilations = [1, 2 , 3]
+        ).to(DEVICE)
+
+        self.ecapa.load_state_dict(torch.load(MODEL_DIR, map_location=DEVICE))
         self.ecapa.eval()
 
     @torch.no_grad()
-    def encode(self, features: torch.Tensor) -> torch.Tesnor:
-        emb = self.ecapa(features)
-        emb = torch.nn.functional.normalize(emb, dim=-1)
-        return emb
+    def encode(self, mel_features: torch.Tensor) -> torch.Tensor:
+        emb = self.ecapa(mel_features)
+        return emb.squeeze(0).cpu().numpy()
