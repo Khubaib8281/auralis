08c203f4fd45430763ff414c7a4af1f73080587c
Muhammad Khubaib Ahmad <muhammadkhubaibahmad854@gmail.com>
2026-01-04 14:56:38 +0500

    gitignore updated

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,5 +1,9 @@
-from fastapi.testclient import TestClient
+import os
+import sys
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 from main import app
+from fastapi.testclient import TestClient
 
 client = TestClient(app)
 
@@ -9,5 +13,5 @@ def test_health_endpoint():
 
 def test_exception_case():
     with open("tests/invalid.txt", "rb") as f:
-        response = client.post("api/v1/voice/score", files = {"file", :f})
+        response = client.post("/api/v1/voice/score", files = {"file": f})
     assert response.status_code == 400
